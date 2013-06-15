f59cad07b81c158729fead36ea7a77c6deab5a48
Allison <amoses016@gmail.com>
2013-06-15 19:08:54 +0000

    Shelter struct file

---
+++ perfectpet4me/shelter/Shelter.go
@@ -0,0 +1,34 @@
+package main
+
+
+import "fmt"
+
+
+type Shelter struct {
+	name string
+	phone string
+	email string
+	ID
+	address1
+	address2
+	state
+	zip
+}
+
+
+func NewShelter (newName, newPhone, newEmail, newID, newAddress1, newAddress2, newState, newZip) Shelter* {
+	newShelter := new(Shelter)
+
+	newShelter.name = newName
+	newShelter.phone = newPhone
+	newShelter.email = newEmail
+	newShelter.ID = newID
+	newShelter.address1 = newAddress1
+	newShelter.address2 = newAddress2
+	newShelter.state = newState
+	newShelter.zip = newZip
+
+	return newShelter
+}
+
+
