3d0458b2f8662f71b0b592be6a5fff449b1a3418
Allison <amoses016@gmail.com>
2013-06-15 19:48:19 +0000

    Fixed package name and capitalized struct variables

--- perfectpet4me/pet/Pet.go
+++ perfectpet4me/pet/Pet.go
@@ -1,30 +1,30 @@
-package main
+package pet
 
 
 import "fmt"
 
 
 type Pet struct {
-	name string 
-	age string 			//Baby, Young, Adult, or Senior
-	sex string 			// M or F
-	animalType string 	//Dog, Cat, Small&Furry, BarnYard, Bird, Horse, Pig, Rabbit, or Reptile
-	breed string
-	size string			//S, M, L, or XL
-	shelterID int		//ID of shelter the pet is at
+	Name string 
+	Age string 			//Baby, Young, Adult, or Senior
+	Sex string 			// M or F
+	AnimalType string 	//Dog, Cat, Small&Furry, BarnYard, Bird, Horse, Pig, Rabbit, or Reptile
+	Breed string
+	Size string			//S, M, L, or XL
+	ShelterID int		//ID of shelter the pet is at
 }
 
 
 func NewPet (newName, newAge, newSex, newAnimalType, newBreed, newSize string, newShelterID int) *Pet {
 	newPet := new(Pet)
 
-	newPet.name = newName
-	newPet.age = newAge
-	newPet.sex = newSex
-	newPet.animalType = newAnimalType
-	newPet.breed = newBreed
-	newPet.size = newSize
-	newPet.shelterID = newShelterID
+	newPet.Name = newName
+	newPet.Age = newAge
+	newPet.Sex = newSex
+	newPet.AnimalType = newAnimalType
+	newPet.Breed = newBreed
+	newPet.Size = newSize
+	newPet.ShelterID = newShelterID
 
 	return newPet
 }
@@ -33,6 +33,6 @@ func NewPet (newName, newAge, newSex, newAnimalType, newBreed, newSize string, n
 /*func main () {
 	pet := NewPet("Fluffy", "Young", "M", "Dog", "Doberman", "L", 123456)
 
-	fmt.Println(pet.name)
-	fmt.Println(pet.shelterID)
+	fmt.Println(pet.Name)
+	fmt.Println(pet.ShelterID)
 }*/
