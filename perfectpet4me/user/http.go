5767ca398b16aa954f7f01480643e64a72bf3182
Jason Pack <jason482@gmail.com>
2013-06-15 22:43:40 -0500

    Conditional display of zip input field.

--- perfectpet4me/user/http.go
+++ perfectpet4me/user/http.go
@@ -10,6 +10,7 @@ import (
 
 func init() {
 	http.HandleFunc("/user", handler)
+	http.HandleFunc("/user/save", save)
 }
 
 var userTmpls = template.Must(template.ParseFiles("templates/base.html", "perfectpet4me/user/templates/user.html"))
@@ -29,4 +30,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err := userTmpls.Execute(w, u); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func save(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	aeu := aeuser.Current(c)
+	k := datastore.NewKey(c, "user", aeu.Email, 0, nil)
+
+	var u User
+
+	if err := datastore.Get(c, k, &u); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	u.Zip = r.FormValue("zip")
+	datastore.Put(c, k, &u)
 }
