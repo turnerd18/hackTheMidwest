c83ebdee5a543f94d51e5582a6c795ebca189c72
Devin <turnerd18@gmail.com>
2013-06-20 22:32:07 -0500

    added a test for the petfinder library

--- perfectpet4me.py
+++ perfectpet4me.py
@@ -1,6 +1,8 @@
 import os
 import urllib
 
+import petfinder
+
 from google.appengine.api import users
 
 import jinja2
@@ -17,6 +19,16 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('templates/index.html')
         self.response.write(template.render())
 
+class TestPage(webapp2.RequestHandler):
+
+    def get(self):
+        # Instantiate the client with your credentials.
+        api = petfinder.PetFinderClient(api_key='4b7c674b10cfc0793e935e8abd7346c2', api_secret='15e0e406bc3bcc284773a57957492133')
+        pet = api.pet_getrandom()
+        self.response.write(pet['name'])
+
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/test', TestPage),
 ], debug=True)
